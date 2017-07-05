ab8da9e38d4aa9e8d7eb0499c33a7e3981fa5af5
Jonatan da Hora <jonatandahora@gmail.com>
2017-07-05 02:02:36 -0300

    created basic app and model Task

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChecklistBackend::Application.config.secret_token = '3688e357c8de19c3700f95eacd77ba467490e756890c417ae61d669f52526736d21fc8f6280694802828cb413815a005c7b7b1f2f39e3d27bc56ce992a25feff'
